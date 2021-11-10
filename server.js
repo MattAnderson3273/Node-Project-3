711798cb483d32a5cacacbc7baa77513233bdf5e
Zachary Doctrove <zdoctrov@umich.edu>
2021-11-10 14:10:05 -0500

    Push Test (ZacDoc)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 5000; 
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World'); 
+    res.end('Cyanara Earth'); 
 }); 
 
 server.listen(port, () => console.log(`Server listening on port ${port}`)); 
