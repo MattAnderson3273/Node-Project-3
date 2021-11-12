68505ce1679e469be6edb45b3ed6fde1bc9e7c4e
Matt <50434895+MattAnderson3273@users.noreply.github.com>
2021-11-12 17:21:00 -0500

    Set up Express server, add and retrieve notes

---
+++ notes.js
@@ -0,0 +1,70 @@
+const express = require('express'); 
+const router = express.Router(); 
+const fs = require('fs'); 
+const path = require('path'); 
+
+// @route   GET /notes
+// @desc    Get notes from text files
+// @access  Public
+router.get('/', (req, res) => {
+    const { username } = req.body; 
+    if(username) {
+        try {
+            fs.readFile(path.join(__dirname, '/usernames', `${username}.txt`), 'utf8', (error, data) => {
+                if (error) {
+                    res.status(404).send({ msg: `No notes for ${username} found.` }); 
+                    return;
+                } 
+
+                res.status(200).send({
+                   username: `${username}`,
+                   notes: data
+                });
+            });
+        } catch (err) {
+            res.status(500).send('Server Error'); 
+            console.error(err.message); 
+        }
+    } 
+    else {
+        res.status(404).send({ msg: 'Please provide a username' });     
+    }
+});
+
+// @route   POST /notes
+// @desc    Show note and its contents
+// @access  Public
+router.post('/', (req, res) => { 
+    const { username, note } = req.body;  
+    if(username && note) {
+        try {
+            fs.appendFile(path.join(__dirname, '/usernames', `${username}.txt`), `${note}\n`, error => {
+                if (error) throw error; 
+                res.status(200).send({ msg: `${username}.txt has been written to!` }); 
+            }); 
+        } catch (err) {
+            res.status(500).send('Server Error'); 
+            console.error(err.message); 
+        }
+    }
+    else {
+        res.status(404).send({ msg: 'Please provide a username and a note' }); 
+    }
+});
+
+// @route   PATCH /notes
+// @desc    Update note 
+// @access  Public
+// IMPORTANT: Not sure if we will end up using patch 
+router.patch('/', (req, res) => {
+    res.status(200).send('Hello World!'); 
+});
+
+// @route   DELETE /notes
+// @desc    Delete a note from the file 
+// @access  Public
+router.delete('/', (req, res) => {
+    res.status(200).send('Hello World!'); 
+});
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,40 +1,15 @@
 const fs = require('fs');
-const usernameFolder = 'usernames';
+const express = require('express'); 
 
-fs.readdirSync(usernameFolder).forEach(file => {
-  console.log(file);
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+// JSON Body Parser
+app.use(express.json()); 
+
+// Notes Route
+app.use('/notes', require('./notes')); 
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`); 
 });
-
-
-// All the node stuff happen in the server
-//  Need to establish a connection between client & server (Fetch requests)
-
-// Need to make it so it understands these requests
-// Add
-// Edit
-// Delete
-
-// After receiving a username from the client, 
-// the server should check to see if a textfile has the same name and read from it.
-// If not, just create a new file and begin writing to it (If its a "View Note" request, show nothing due to the filtering)
-
-
-// try {
-//   const data = fs.readFileSync('', 'utf8')
-//   console.log(data)
-// } catch (err) {
-//   console.error(err)
-// }
-
-const http = require('http'); 
-const port = 5000; 
-
-const server = http.createServer((req, res) => {
-
-    res.end('Cyanara Earth'); 
-}); 
-
-server.listen(port, () => console.log(`Server listening on port ${port}`));
-
-// Receive data from client.js
-server.addListener()
